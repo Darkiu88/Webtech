0d753cb8f9f18c90997a4a1f1e8dcf45f8ce078f
frontendninja83 <frontendninja83@gmail.com>
2025-02-26 20:03:28 -0600

    Fix ALLOWED_HOSTS for Render

--- Greentechpc/settings.py
+++ Greentechpc/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-p4r=+$6i1nq*o7o&s#-y5*_+n=5hd0u4s4t=-yw+i_a%od@65*
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['webtech-pyvw.onrender.com']
 
 
 # Application definition
